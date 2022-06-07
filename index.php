429b33945bfa0b392509dba8b7524b6681a39bb7
Lolloc99 <lolloc99@yahoo.it>
2022-06-07 17:31:29 +0200

    Esercizio concluso

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo "Hello world dsdfsdf " .  $_GET['nome'];
+$testo = "Se l’arcivescovo di Costantinopoli si disarcivescoviscostantinopolizzasse, tu ti disarcivescoviscostantinopolizzeresti come si è disarcivescoviscostantinopolizzato l’arcivescovo di Costantinopoli?";
+$badword = $_GET['badword'];
+$testo_riadattato = str_replace($badword, '***', $testo);
 ?>
+
+<h2>Questo è il paragrafo di prova:</h2>
+<h3><?php echo $testo . $prova; ?></h3>
+
+<h2>La lunghezza del testo è di: <?php echo strlen($testo);?> caratteri</h2>
+
+<h2>Badword: </h2>
+<h3><?php echo $badword;?></h3>
+
+<h2>Testo riadattato</h2>
+<h3>
+    <?php echo $testo_riadattato;?>
+</h3>
+
+<h2>La lunghezza del testo riadattato è di: <?php echo strlen($testo_riadattato);?> caratteri</h2>
